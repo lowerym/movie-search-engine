e11467d1c9fc8d30bcfc66b9f5aa55ca6e3d4132
aahmad1123 <ayat.asaad@gmail.com>
2024-02-01 21:20:26 -0600

    commented out test fetches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 var fetchButton = document.getElementById("fetch-button")
 
 
-fetch('https://api.themoviedb.org/3/movie/11?api_key=12126786fe2ba8d56422edd3325172f9')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// fetch('https://api.themoviedb.org/3/movie/11?api_key=12126786fe2ba8d56422edd3325172f9')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
 
-  fetch('http://www.omdbapi.com/?apikey=c236aea6&t=the+godfather')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+//   fetch('http://www.omdbapi.com/?apikey=c236aea6&t=the+godfather')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
 
   function getAPI(title){
     fetch ("http://www.omdbapi.com/?apikey=c236aea6&t="+ title)
