20e08cf20bf63d1c6706a39abab6629040d18789
Matthew Lowery <matthewdlowery@msn.com>
2024-02-05 21:21:30 -0600

    Built out index page, created and added new stylesheet, and worked on script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,16 @@ function getAPI(title){
     return response.json();
   })
   .then(function (data) {
-    console.log(data.Year);
-    var display = document.getElementById("display")
-    var movieTitle = data.Title
-    var movieYear = data.Year
-    display.append(movieTitle + " "+ movieYear)
+    console.log(data);
+    var displayInfo = document.getElementById("displayInfo");
+    var movieTitle = data.Title;
+    var movieYear = data.Year;
+    var movieDirector = data.Director;
+    var movieWriter = data.Writer;
+    var movieCast = data.Actors;
+    var movieSynopsis = data.Plot;
+    var html = "<h2 class='text-2xl font-bold'>" + movieTitle + " " + "(" + movieYear + ")</h2><br/><h3 class='text-xl font-semibold'>Directed by:</h3><p>" + movieDirector + "</p><br/><h3 class='text-xl font-semibold'>Written by:</h3><p>" + movieWriter + "</p><br/><h3 class='text-xl font-semibold'>Starring:</h3><p>" + movieCast + "</p><br/><h3 class='text-xl font-semibold'>Synopsis:</h3><p>" + movieSynopsis + "</p>";
+    displayInfo.innerHTML = html;
     });
 }
 
@@ -23,17 +28,20 @@ function getAPI2(title){
   })
   .then (function(data) {
     console.log(data);
-    display.innerHTML= ""
+    console.log(data.video);
+    var displayPoster = document.getElementById("displayPoster");
     console.log(data.results[0].poster_path)
     var posterLink = data.results[0].poster_path
     var moviePoster = document.createElement("img")
     moviePoster.setAttribute("src", "https://image.tmdb.org/t/p/original" + posterLink)
-    display.append(moviePoster)
+    displayPoster.append(moviePoster);
   });
 }
 
 fetchButton.addEventListener("click", function(){
+  event.preventDefault();
   var title = document.querySelector("#title").value
   getAPI(title)
   getAPI2(title)
+  display.classList.remove('hidden');
 })
